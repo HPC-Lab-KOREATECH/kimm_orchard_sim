ccb93d9c7347af98f14f24b4d3660dea0919c10f
dbsghdud07 <dbsghdud5961@gmail.com>
2024-02-12 11:19:03 +0000

    feat: dockerfile, LIO-SAM bug

--- kimm_orchard_sim/docker/create_docker.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-docker pull althack/ros2:humble-cuda-gazebo-nvidia-2023-11-05
-
-docker build -t kimm_orchard_gazebo:v1 .
-
-docker run -it --privileged -e DISPLAY=$DISPLAY --env="QT_X11_NO_MITSHM=1" -v="/tmp/.gazebo/:/root/.gazebo/" -v /tmp/.X11-unix:/tmp/.X11-unix:ro -v /home/hy/docker_mounted/kimm_orchard/:/root/mounted_folder/ --hostname hy-B660M-HD3P --network host --gpus all --name kimm_orchard_v1 kimm_orchard_gazebo:v1 bash
