8132aedf2ddce8a1ce618b139eecae4324ea7a05
dbsghdud07 <dbsghdud07@gmail.com>
2024-04-26 16:46:32 +0900

    run_command edit

--- kimm_orchard_sim/docker/run_command.sh
+++ kimm_orchard_sim/docker/run_command.sh
@@ -1,6 +1,5 @@
+#!/bin/bash
 set -e
-
-docker pull althack/ros2:humble-cuda-gazebo-nvidia-2023-11-05
 
 docker build -t kimm:v1.0 .
 
@@ -8,11 +7,13 @@ docker run -it \
     --privileged \
     -e DISPLAY=$DISPLAY \
     --env="QT_X11_NO_MITSHM=1" \
-    -v="/tmp/.gazebo/:/root/.gazebo/" \
     -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
-    -v /home/$USER/docker_mounted/kimm_orchard/:/root/mounted_folder/ \
-    --hostname hy-B660M-HD3P \
+    -v /home/$USER/docker_mounted/:/root/mounted_folder/ \
+    -v="/tmp/.gazebo/:/root/.gazebo/" \
+    --hostname $USER \
     --network host \
     --gpus all \
-    --name kimm_orchard \
+    --ipc=host \
+    -P \
+    --name kimm_orchard
     kimm:v1.0 bash
